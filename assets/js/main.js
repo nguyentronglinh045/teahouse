f7f604d262c13203fa56a8ba69f99176e027c5df
Trong Linh <nguyentronglinh045@gmail.com>
2022-04-08 18:36:24 +0700

    nội dụng đã thực hiện

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,11 +1,56 @@
-var formSearch = document.getElementById('nav__form-search');
-var searchHover = document.getElementById('search-hover');
-console.log(formSearch, searchHover);
+const listProducts = [
+    {
+        id: 1,
+        name: 'Trà phúc bồn tử',
+        price: 45000,
+        image: '../imgs/5.jpg',
+    },
+    {
+        id: 2,
+        name: 'Trà vải',
+        price: 40000,
+        image: '../imgs/12.jpg',
+    },
+    {
+        id: 3,
+        name: 'Trà dâu tây',
+        price: 40000,
+        image: '../imgs/6.jpg',
+    },
+    {
+        id: 4,
+        name: 'Trà chanh',
+        price: 30000,
+        image: '../imgs/11.jpg',
+    },
+    {
+        id: 5,
+        name: 'Trà xoài',
+        price: 35000,
+        image: '../imgs/8.jpg',
+    },
+    {
+        id: 6,
+        name: 'Trà táo',
+        price: 30000,
+        image: '../imgs/10.jpg',
+    },
+    {
+        id: 7,
+        name: 'Trà cam đào',
+        price: 45000,
+        image: '../imgs/9.jpg',
+    },
+    {
+        id: 8,
+        name: 'Trà vải',
+        price: 40000,
+        image: '../imgs/12.jpg',
+    },
+];
 
-
-searchHover.addEventListener('mouseover', ()=>{
-    formSearch.classList.remove('active');
-});
-searchHover.addEventListener('mouseout', ()=>{
-    formSearch.classList.add('active');
-});
+const item = listProducts.map(product => {
+    return (`
+        
+    `)
+})
